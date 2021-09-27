51869bf3cbe2c6aaf5f6d8ce31a04ce65b323c85
Rogerio Guimaraes <rogerio.salesg@gmail.com>
2021-09-27 20:50:54 -0300

    Configuracao do ambiente

--- src/test/java/com/estudo/tdd/TddApplicationTests.java
+++ src/test/java/com/estudo/tdd/TddApplicationTests.java
@@ -1,9 +1,17 @@
 package com.estudo.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.security.RunAs;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
 class TddApplicationTests {
 
 	@Test
